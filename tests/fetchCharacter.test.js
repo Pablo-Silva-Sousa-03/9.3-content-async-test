3b223ec6d89f151eb3ded9048fc6cab3b9e1ec01
Renanbfreitas <renanbf1992@outlook.com>
2022-09-09 22:07:42 -0300

    Adicionando exercicio do dia

--- tests/fetchCharacter.test.js
+++ tests/fetchCharacter.test.js
@@ -2,5 +2,8 @@ require('../mocks/fetchSimulator');
 const { fetchCharacter } = require('../src/fetchCharacter');
 
 describe('Teste a função fetchCharacter', () => {
-  it('', () => {});
+  it('Verifica se retorna \'Invalid id\' ao executar a função com parâmetro que não existe', async () => {
+    const response = await fetchCharacter('parâmetro qualquer');
+    expect(response).toBe('Invalid id');
+  });
 });
